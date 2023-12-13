02081e016dacf597ef17734561abf612643594c8
Shakejelly <fredrich93@gmail.com>
2023-12-13 11:28:02 +0100

    Add project files.

---
+++ ThreadLabb2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThreadLabb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
