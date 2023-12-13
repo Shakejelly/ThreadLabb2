64bbf280bbf6e703d6cc2a007ddacc836da66f79
Shakejelly <fredrich93@gmail.com>
2023-12-13 14:55:27 +0100

    added cars in threads

---
+++ ThreadLabb2/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadLabb2
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public Car(string name)
+        {
+            Name = name;
+        }
+        public void Move()
+        {
+
+            for (int i = 0; i <= 14000; i++)
+            {
+                Thread.Sleep(140);
+                
+
+
+                //if (i == 14000)
+                //{
+                //    Console.WriteLine("=================================");
+                //    Console.WriteLine($"140km! {Name} reached the goal!");
+                //    Console.WriteLine("=================================");
+                //    Environment.Exit(0);
+                //}
+
+            }
+        }
+    }
+}
--- ThreadLabb2/Program.cs
+++ ThreadLabb2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Press N to get information");
+
+            Car car1 = new Car("Speedy McZoomZoom");
+            Car car2 = new Car("Vroom Vroom Veggie Mobile");
+            Car car3 = new Car("RaceLord Von Drifty");
+
+            Thread thread1 = new Thread(car1.Move);
+            Thread thread2 = new Thread(car2.Move);
+            Thread thread3 = new Thread(car3.Move);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            
+            
+
         }
+
+
     }
 }
