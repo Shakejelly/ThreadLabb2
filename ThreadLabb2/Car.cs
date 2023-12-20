92b735609bec2c4dc7a1b19a9b1432195cac62ba
Shakejelly <fredrich93@gmail.com>
2023-12-20 16:16:58 +0100

    Yeah boi

--- ThreadLabb2/Car.cs
+++ ThreadLabb2/Car.cs
@@ -9,21 +9,64 @@ namespace ThreadLabb2
     internal class Car
     {
         public string Name { get; set; }
+        public double CurrentDistance { get; set; }
         public Car(string name)
         {
             Name = name;
         }
         public void Move()
         {
-            
-            for (int i = 0; i < 1000; i++)
+
+            double kmH = 110;
+
+            double distance = 10;
+
+
+
+
+            Random random = new Random();
+            int i = 0;
+            while (CurrentDistance < distance)
             {
-                Random random = new Random(1 - 50);
-                
-                Console.WriteLine($@"{random}");
+                i++;
+                if (i % 8 == 0)
+                {
+                    int randomValue = random.Next(1, 50);
+
+                    if (randomValue == 1)
+                    {
+                        Console.WriteLine($"{Name} needed a pitstop to change oil. Estimated time: 30s!");
+                        Thread.Sleep(30000);
+
+                    }
+                    else if (randomValue <= 3)
+                    {
+                        Console.WriteLine($"{Name} needed a pitstop to change tires. Estimated time: 20s.");
+                        Thread.Sleep(20000);
+                    }
+                    else if (randomValue > 3 && randomValue < 10)
+                    {
+                        Console.WriteLine($"Ouch! {Name} just hit a bird! Estimated time: 10s ");
+                        Thread.Sleep(10000);
+                    }
+                    else if (randomValue > 9 && randomValue < 21)
+                    {
+                        Console.WriteLine($"{Name} speed decreased!");
+                        kmH = kmH - 1;
+                    }
+                    
+                }
+
+                Thread.Sleep(1000);
+                CurrentDistance += kmH / (60 * 60); 
             }
+            Console.WriteLine($"{Name} has reached the goal!" );
+            //TODO
+            //Visa roligare UI
+            //Avsluta program n'r man har vunnit.
+            //Skriva ut andra och tredjeplats.
             
         }
-        
+       
     }
 }
--- ThreadLabb2/Program.cs
+++ ThreadLabb2/Program.cs
@@ -4,26 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press N to get information");
+                Car car1 = new Car("Speedy McZoomZoom");
+                Car car2 = new Car("Vroom Vroom Veggie Mobile");
+                Car car3 = new Car("RaceLord Von Drifty");
 
-            Car car1 = new Car("Speedy McZoomZoom");
-            Car car2 = new Car("Vroom Vroom Veggie Mobile");
-            Car car3 = new Car("RaceLord Von Drifty");
+                Thread thread1 = new Thread(car1.Move);
+                Thread thread2 = new Thread(car2.Move);
+                Thread thread3 = new Thread(car3.Move);
 
-            Thread thread1 = new Thread(car1.Move);
-            Thread thread2 = new Thread(car2.Move);
-            Thread thread3 = new Thread(car3.Move);
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
 
-            thread1.Start();
-            thread2.Start();
-            thread3.Start();
+              while (true)
+               {
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"{car1.Name} - Current distance: {Math.Round(car1.CurrentDistance * 100, 0)} m.");
+                Console.WriteLine($"{car2.Name} - Current distance: {Math.Round(car2.CurrentDistance * 100, 0)} km.");
+                Console.WriteLine($"{car3.Name} - Current distance: {Math.Round(car3.CurrentDistance * 100, 0)} km.");
+                Thread.Sleep(100);
+               }
 
-            thread1.Join();
-            thread2.Join();
-            thread3.Join();
+                thread1.Join();
+                thread2.Join();
+                thread3.Join();
+
 
             
-            
+
 
         }
 
